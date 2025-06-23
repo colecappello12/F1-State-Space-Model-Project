c0b8505dc40cb8fe1b92843b2aafca90feddf32c
Cole Cappello <colecappello@Coles-MacBook-Pro.local>
2025-06-22 20:27:55 -0600

    Added full race simulation code for a single driver

--- F1_Data_Generation/f1Data.py
+++ F1_Data_Generation/f1Data.py
@@ -2,14 +2,14 @@ import fastf1 as f1
 import pandas as pd
 
 # Loads race  data from the Bahrain Grand Prix this year
-session = f1.get_session(2025,"Bahrain","R")
+session = f1.get_session(2025,"Imola","R")
 session.load()
 
 # Shows you some info about the event
 print(session.event)
 
 # Makes a dataframe with all lap timing data for the driver Verstappen, Norris, and Leclerc
-laps = pd.DataFrame(session.laps.pick_drivers(["VER","NOR","LEC"]))
+laps = pd.DataFrame(session.laps.pick_drivers(["VER","PIA","LEC","RUS","HAM"]))
 
 def to_secs(row):
     tSecs = row['LapTime'].total_seconds() 
@@ -21,6 +21,6 @@ laps['LapTime'] = laps.apply(to_secs,axis = 1)
 # Makes a dataframe with telemetry data for Chalres Leclerc
 lec_telm = pd.DataFrame(session.laps.pick_drivers("LEC").get_telemetry())
 
-laps.to_csv("~/Code/Python_Programs/F1_Data_Generation/bahrain_laps.csv")
+laps.to_csv("~/Code/F1_Project/F1_Data_Generation/imola_laps.csv")
 
-lec_telm.to_csv("~/Code/Python_Programs/F1_Data_Generation/lec_telm.csv")
+# lec_telm.to_csv("~/Code/Python_Programs/F1_Data_Generation/lec_telm.csv")
